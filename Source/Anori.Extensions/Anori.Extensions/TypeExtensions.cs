1e22ddab9e0a7601c8f83af1a099b93393f86ce3
Martin Egli <martin.egli@reishauer.com>
2021-02-26 10:46:15 +0100

    Revert "Update folder structure"
    This reverts commit 4508e672d0f1a470098a1a205e9d1cdf9ddac695.

---
+++ Source/Anori.Extensions/Anori.Extensions/TypeExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace Anori.Extensions
+{
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Determines whether this instance is nullable.
+        /// </summary>
+        /// <param name="genericTypeInfo">The generic type information.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified generic type information is nullable; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">genericTypeInfo</exception>
+        public static bool IsNullable([NotNull] this TypeInfo genericTypeInfo)
+        {
+            if (genericTypeInfo == null)
+            {
+                throw new ArgumentNullException(nameof(genericTypeInfo));
+            }
+
+            return Nullable.GetUnderlyingType(genericTypeInfo) != null;
+        }
+
+        /// <summary>
+        /// Determines whether this instance is nullable.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified type is nullable; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">type</exception>
+        public static bool IsNullable([NotNull] this Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            return Nullable.GetUnderlyingType(type) != null;
+        }
+
+        /// <summary>
+        /// Determines whether this instance is nullable.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>
+        ///   <c>true</c> if this instance is nullable; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsNullable<T>()
+        {
+            return typeof(T).IsNullable();
+        }
+    }
+}
