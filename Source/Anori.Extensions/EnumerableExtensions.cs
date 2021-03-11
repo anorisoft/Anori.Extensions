792eb97f940565d5cb1f54ac17951b1f8d5e230d
Martin Egli <martin.egli@reishauer.com>
2021-03-11 19:43:51 +0100

    Update

--- Source/Anori.Extensions/EnumerableExtensions.cs
+++ Source/Anori.Extensions/EnumerableExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="EnumerableExtensions.cs" company="Anori Soft">
-// Copyright (c) Anori Soft. All rights reserved.
+// <copyright file="EnumerableExtensions.cs" company="AnoriSoft">
+// Copyright (c) AnoriSoft. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
 
--- Source/Anori.Extensions/ValueTypeExtensions.cs
+++ Source/Anori.Extensions/ValueTypeExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="ValueTypeExtensions.cs" company="Anori Soft">
-// Copyright (c) Anori Soft. All rights reserved.
+// <copyright file="ValueTypeExtensions.cs" company="AnoriSoft">
+// Copyright (c) AnoriSoft. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
 
