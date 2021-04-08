d3544ad9fbe17947af043f437cb1887d873d9da8
Martin Egli <martin.egli@reishauer.com>
2021-04-08 20:54:28 +0200

    Add Threading

---
+++ Source/Anori.Extensions/Threading/IDispatchableContext.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IDispatchableContext.cs" company="AnoriSoft">
+// Copyright (c) AnoriSoft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Anori.Extensions.Threading
+{
+    using System.Threading;
+
+    /// <summary>
+    /// Dispatchable Context Interface.
+    /// </summary>
+    public interface IDispatchableContext
+    {
+        /// <summary>
+        /// Gets the synchronization context.
+        /// </summary>
+        /// <value>
+        /// The synchronization context.
+        /// </value>
+        SynchronizationContext SynchronizationContext { get; }
+    }
+}
