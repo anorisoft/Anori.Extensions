196fce1dc4ea3e620c6c631436e21419701d374a
Martin Egli <martin.egli@reishauer.com>
2021-04-17 21:57:32 +0200

    Update ExpressionExtensions

---
+++ Tests/Anori.Extensions.UnitTests/TestClasses/Bindable.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace Anori.ExpressionObservers.UnitTests.TestClasses
+{
+    public abstract class Bindable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
